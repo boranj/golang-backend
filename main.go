6a01b08e797c5fb6baafc4c40ea4f8d0f5aa00d4
boranj <egeboran2012@hotmail.com>
2024-12-25 21:48:20 +0300

    added new handlers for new pages

--- main.go
+++ main.go
@@ -5,11 +5,23 @@ import (
 	"net/http"
 )
 
+func healthz(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type:", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK\n"))
+
+}
+
 func main() {
 	mux := http.NewServeMux()
 
 	fileHandler := http.FileServer(http.Dir("."))
-	http.Handle("GET /", fileHandler)
+	mux.Handle("/app/", http.StripPrefix("/app", fileHandler))
+	imageHandler := http.FileServer(http.Dir("./assets"))
+	mux.Handle("/assets", http.StripPrefix("/assets", imageHandler))
+
+	mux.HandleFunc("GET /healthz", healthz)
+
 	log.Println("creating server at:8080")
 
 	err := http.ListenAndServe(":8080", mux)
