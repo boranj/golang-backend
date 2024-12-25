3faae68fcc7463a2928f7d1c06bd71fc4419384e
boranj <egeboran2012@hotmail.com>
2024-12-25 20:57:48 +0300

    Added homepage html file

--- main.go
+++ main.go
@@ -8,11 +8,12 @@ import (
 func main() {
 	mux := http.NewServeMux()
 
+	fileHandler := http.FileServer(http.Dir("."))
+	http.Handle("GET /", fileHandler)
 	log.Println("creating server at:8080")
 
 	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 }
